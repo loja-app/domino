6dd56bfbb44bdbfefce47b8e99b62099191348dd
app <rodrigo-matos@pm.me>
2025-01-09 03:00:14 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,40 @@
+const CACHE_NAME = 'placar-domino-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png',
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('my-cache').then((cache) => {
-      return cache.addAll([
-        '/', 
-        '/index.html', 
-        '/manifest.json', 
-        '/icon.png'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Arquivos em cache');
+      return cache.addAll(ASSETS);
     })
   );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  console.log('Service Worker ativado!');
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
-      return caches.match('/index.html');
+      return response || new Response('Offline e recurso não encontrado!', {
+        status: 404,
+        statusText: 'Not Found',
+      });
     })
   );
 });
