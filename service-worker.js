94c3932047fe8dde660665290574b1f2c4472045
app <rodrigo-matos@pm.me>
2025-01-08 23:18:48 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,10 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      if (response) {
+        return response;
+      }
+      return caches.match('/index.html');
     })
   );
 });
