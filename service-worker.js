be9284277402e93cca129a5ca51d6165a852add0
app <rodrigo-matos@pm.me>
2025-01-08 23:08:48 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,20 @@
-const CACHE_NAME = 'offline-app-v1';
-const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon.png'
-];
-
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/', 
+        '/index.html', 
+        '/manifest.json', 
+        '/icon.png'
+      ]);
     })
   );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
-      return caches.match('/index.html');
+      return response || fetch(event.request);
     })
   );
 });
