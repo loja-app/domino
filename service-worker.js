8c8e42b9a9bd30f67d32cf7c6536da1829d43cec
app <rodrigo-matos@pm.me>
2025-01-08 20:14:00 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,11 @@
-const CACHE_NAME = 'meu-app-cache-v1';
-const ASSETS_TO_CACHE = [
-  '/domino/',
-  '/domino/index.html',
-  '/domino/icon.png',
-  '/domino/undo.svg'
-];
-
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
-                  .map(cacheName => caches.delete(cacheName))
-      );
+    caches.open('pwa-cache').then(cache => {
+      return cache.addAll([
+        '/index.html',
+        '/icon.png',
+        '/manifest.json'
+      ]);
     })
   );
 });
@@ -29,7 +13,7 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || caches.match('/domino/index.html');
+      return response || fetch(event.request);
     })
   );
 });
